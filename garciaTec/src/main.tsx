84410b71a3de1a5ea0b0c3789234498590bc07ed
Devalyo <danieldevalho@gmail.com>
2025-06-24 21:23:00 -0300

    feat: setup routes, and fixed port

--- garciaTec/src/main.tsx
+++ garciaTec/src/main.tsx
@@ -1,8 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
+import './index.css';
+import Home from "./pages/Home.tsx";
+import Pedidos from './pages/Pedidos.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/pedidos" element={<Pedidos />} />
+    </Routes>
+  </BrowserRouter>
+);
---
+++ garciaTec/src/pages/Pedidos.tsx
@@ -0,0 +1,8 @@
+
+function Pedidos() {
+  return (
+    <div>Pedidos</div>
+  )
+}
+
+export default Pedidos
